74b6957031fd1de78656efa0c5c5c62a82cfbb52
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-01-11 22:44:57 +0100

    deposit works

--- contracts/App.sol
+++ contracts/App.sol
@@ -6,8 +6,27 @@ contract App {
     string public name = "P2P Lending Dapp";
     FakeToken public fakeToken;
 
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStacked;
+    mapping(address => bool) public isStacking;
+    address[] public stackers;
+
     constructor(FakeToken _fakeToken) public {
         fakeToken = _fakeToken;
+    }
+
+    function deposit(uint _amount) public {
+        fakeToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] += _amount; 
+        if (!hasStacked[msg.sender]) {
+            stackers.push(msg.sender);
+        }
+        isStacking[msg.sender] = true;
+        hasStacked[msg.sender] = true;
+    }
+
+    function widthdraw(uint _amount) public {
+        fakeToken.transferFrom(address(this), msg.sender, _amount);
     }
 
 }
--- test/App.test.js
+++ test/App.test.js
@@ -1,14 +1,38 @@
-const App = artifacts.require("App");
-const FakeToken = artifacts.require("FakeToken");
+const App = artifacts.require('App');
+const FakeToken = artifacts.require('FakeToken');
 
-contract("App Test", async (accounts) => {
-  it("should have a name", async () => {
-    const app = await App.deployed();
-    const name = await app.name();
-    assert.equal(name, "P2P Lending Dapp");
-  });
-  it("should transfer", async () => {
-    const app = await App.deployed();
-    const fakeToken = await FakeToken.deployed();
-  });
+const toWei = (value) => {
+    return web3.utils.toWei(value, 'ether');
+};
+
+contract('App Test', async ([owner, investor]) => {
+    let app, token;
+    before(async () => {
+        token = await FakeToken.new();
+        app = await App.new(token.address);
+
+        await token.transfer(app.address, toWei('100000'), { from: owner });
+        await token.transfer(investor, toWei('1000'), { from: owner });
+    });
+
+    it('should have a name', async () => {
+        const name = await app.name();
+        assert.equal(name, 'P2P Lending Dapp');
+    });
+    it('should have funds', async () => {
+        const balance = await token.balanceOf(investor);
+        assert.equal(balance, toWei('1000'));
+    });
+
+    describe('Deposit tokens', async () => {
+        it('deposit should work!', async () => {
+            const balanceBefore = await token.balanceOf(investor);
+            assert.equal(balanceBefore, toWei('1000'), 'balance before is not correct');
+            await token.approve(app.address, toWei('1000'), { from: investor });
+            await app.deposit(toWei('1000'), { from: investor });
+
+            const balanceAfter = await token.balanceOf(investor);
+            assert.equal(balanceAfter, toWei('0'));
+        });
+    });
 });
